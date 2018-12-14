b4d462deb91b034e29621afa66ca54d96a96d55d
justynias <justynakozik1@gmail.com>
2018-12-14 15:40:06 +0000

    initial daos

--- app/src/main/java/mvvm/coding/story_it/MainActivity.kt
+++ app/src/main/java/mvvm/coding/story_it/MainActivity.kt
@@ -2,11 +2,13 @@ package mvvm.coding.story_it
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import mvvm.coding.story_it.data.db.PlayerDao
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
