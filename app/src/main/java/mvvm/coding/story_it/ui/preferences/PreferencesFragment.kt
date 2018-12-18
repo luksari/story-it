d5b96bccf8ebedd523d1c1ca9b7d13346ed5907b
luksari <luksari23@gmail.com>
2018-12-18 15:00:52 +0000

    Finished layout for preferences fragment

--- app/src/main/java/mvvm/coding/story_it/ui/preferences/PreferencesFragment.kt
+++ app/src/main/java/mvvm/coding/story_it/ui/preferences/PreferencesFragment.kt
@@ -1,11 +1,12 @@
 package mvvm.coding.story_it.ui.preferences
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
+import org.koin.android.viewmodel.ext.android.viewModel
 
 import mvvm.coding.story_it.R
 
@@ -15,7 +16,7 @@ class PreferencesFragment : Fragment() {
         fun newInstance() = PreferencesFragment()
     }
 
-    private lateinit var viewModel: PreferencesViewModel
+    private val viewModel: PreferencesViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +27,6 @@ class PreferencesFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(PreferencesViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
